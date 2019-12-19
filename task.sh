9eab940886442b669294cb53a8d9ac685104a9d5
Madhumitha Sakthi <msakthi@cs.utexas.edu>
2019-12-19 12:35:40 -0600

    changes task file

--- task.sh
+++ task.sh
@@ -1,3 +1,4 @@
 #/usr/bin/env bash
 
-python3 sentiment.py --model FANCY --eval true --blind_test_path 'data/enes_test.txt' 
+python3 sentiment.py --model FANCY 
+#--eval true --blind_test_path 'data/enes_test.txt' 
